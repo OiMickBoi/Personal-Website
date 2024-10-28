0ce8716d6949af938892e0cd9a1381b4d87c6a2c
Joshua Boyle <oimickboi@gmail.com>
2024-10-28 00:59:34 -0600

    updates

--- react-portfolio/src/components/Projects/ProjectCard.jsx
+++ react-portfolio/src/components/Projects/ProjectCard.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import project from '../../data/projects.json';
+import { getImageUrl } from "../../utils";
 
-export const Projects = () => {
-  return
+export const ProjectCard = ({ 
+  project:  { title, imageSrc, description, skills, demo, source}, 
+}) => {
+  return (
+    <div>
+      <img 
+        src={getImageUrl(imageSrc)} 
+        alt={`Image of ${title}`} 
+      />
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <ul>
+        {skills.map((skill, id) => {
+          return (
+            <li key={id}>
+              {skill}
+            </li>
+          );
+        })}
+      </ul>
+      <div>
+        <a href={demo}>Demo</a>
+        <a href={source}>Source</a>
+      </div>
+    </div>
+  );
 };
--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -1,35 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import project from '../../data/projects.json';
+import projects from "../../data/projects.json";
+import {ProjectCard} from "./ProjectCard";
 
 export const Projects = () => {
   return (
     <section>
-      <h2>
-        <div>{ projects.map((project, id) =>{
-          
-          return (
-              <div key={id}> 
-                <img src={project.imageSrc} alt={`Image of ${project.title}`} />
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-                <ul>{
-                  projects.skills.map((skill, id) => {
-                    <li key={id}>{skill}</li>;
-                  })
-                
-                }</ul>
-                <div>
-                  <img src={getImageUrl(project.imageSrc)} />
-                  <a href={project.demo}>Demo</a>
-                  <a href={project.source}>Source</a>
-
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </h2>
+      <h2>Projects</h2>
+      <div>
+        { projects.map((project, id) =>{
+          return <ProjectCard key={id} project={project} />;
+        })}
+      </div>
     </section>
   );
-}
+};
